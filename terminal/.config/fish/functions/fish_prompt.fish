498671e6c73b4b507d2300a28bd46448be036920
Jack Guinane <jackguinane@gmail.com>
2024-03-13 23:56:38 -0700

    Make prompt more effecient

--- terminal/.config/fish/functions/fish_prompt.fish
+++ terminal/.config/fish/functions/fish_prompt.fish
@@ -2,27 +2,29 @@ function fish_prompt
 
     switch $status
         case 0
-            set arrow (set_color green)'→'
+            set arrow (set_color green)'→ '
         case '*'
-            set arrow (set_color red)'→'
+            set arrow (set_color red)'→ '
     end
 
-    set -l git_repo (command git --no-optional-locks rev-parse --show-toplevel 2>/dev/null || echo)
+    if test $PWD != (echo $__CACHED_PWD || echo)
 
-    if test -n $git_repo
-        
-        set -l path_to_git_parent (command git --no-optional-locks rev-parse --show-cdup 2>/dev/null)
-        set -l distance_to_git (math (string match -ar '/' $path_to_git_parent | count) + 1)
+        # avoiding this work when we've already cached it saves ~24ms
+        set -l git_repo (command git --no-optional-locks rev-parse --show-toplevel 2>/dev/null || echo)
 
-        set current_dir (prompt_pwd -d 0 | string split --right --max $distance_to_git '/')
-        set --erase current_dir[1]
-        
-    else
-        set current_dir (prompt_pwd -d 0 | string split '/')
+        if test -n $git_repo    
+            set -l path_to_git_parent (command git --no-optional-locks rev-parse --show-cdup 2>/dev/null)
+            set -l distance_to_git (math (string match -ar '/' $path_to_git_parent | count) + 1)
+            set current_dir (prompt_pwd -d 0 | string split --right --max $distance_to_git '/')
+            set --erase current_dir[1]
+        else
+            set current_dir (prompt_pwd -d 0 | string split '/')
+        end
+
+        set current_dir[-1] (set_color magenta)$current_dir[-1]
+        set -gx __CACHED_PWD $PWD
+        set -gx __CACHED_PROMPT (set_color brblack)(string join '/' $current_dir)
     end
-
-    set current_dir[-1] (set_color normal --dim)$current_dir[-1]
-    
-    set_color brblack
-    echo (string join '/' $current_dir) $arrow (set_color normal)
+        
+    echo $__CACHED_PROMPT $arrow
 end
--- terminal/.config/fish/functions/fish_right_prompt.fish
+++ terminal/.config/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,3 @@
 function fish_right_prompt
-    echo (set_color brblack)(date '+%b %e %l:%M' | string split " " --no-empty)
+    echo (set_color brblack)(date '+%b %e, %l:%M' | string split ' ' --no-empty)
 end
