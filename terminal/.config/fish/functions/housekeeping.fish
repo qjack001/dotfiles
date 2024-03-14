558541938180c544cbaab20563c479a2a7d56cad
Jack Guinane <jackguinane@gmail.com>
2024-03-13 23:57:46 -0700

    update fish functions

--- terminal/.config/fish/functions/housekeeping.fish
+++ terminal/.config/fish/functions/housekeeping.fish
@@ -1,6 +1,10 @@
 function housekeeping --description 'Do cleanup chores for brew installs'
+    set -l brewfile ~/dotfiles/Brewfile
     brew update &&
     brew upgrade &&
     brew cleanup &&
-    brew autoremove
+    brew autoremove &&
+    brew bundle dump --file=$brewfile --force --describe &&
+    brew bundle install --file=$brewfile &&
+    fish_update_completions
 end
---
+++ terminal/.config/fish/functions/tabs.fish
@@ -0,0 +1,3 @@
+function tabs --wraps=zellij --description 'Enter a multi-pane setup'
+    zellij $argv
+end
